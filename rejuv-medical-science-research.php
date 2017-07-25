3effb3b75342ee282b0b11760979eba6c4a1a254
Andrew Minion <andrew@andrewrminion.com>
2017-07-25 18:15:01 -0400

    reorder filters

--- rejuv-medical-science-research.php
+++ rejuv-medical-science-research.php
@@ -195,21 +195,21 @@ add_action( 'after_setup_theme', 'rejuv_science_options' );
  * @param  string $path ACF local JSON save directory
  * @return string ACF local JSON save directory
  */
-add_filter( 'acf/settings/save_json', 'rejuv_science_acf_json_save_point' );
 function rejuv_science_acf_json_save_point( $path ) {
     return plugin_dir_path( __FILE__ ) . '/acf-json';
 }
+add_filter( 'acf/settings/save_json', 'rejuv_science_acf_json_save_point' );
 
 /**
  * Set ACF local JSON open directory
  * @param  array $path ACF local JSON open directory
  * @return array ACF local JSON open directory
  */
-add_filter( 'acf/settings/load_json', 'rejuv_science_acf_json_load_point' );
 function rejuv_science_acf_json_load_point( $path ) {
     $paths[] = plugin_dir_path( __FILE__ ) . '/acf-json';
     return $paths;
 }
+add_filter( 'acf/settings/load_json', 'rejuv_science_acf_json_load_point' );
 
 /**
  * Add search form to science/research menu
